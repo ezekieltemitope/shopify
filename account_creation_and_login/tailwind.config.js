c012eaa4a98fd63f6f0a99b5546fd851306b95a3
BigDex101 <ogbeborderry@gmail.com>
2024-06-05 15:37:40 +0100

    added account creation and login

---
+++ account_creation_and_login/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#40c040'
+      }
+    },
+  },
+  plugins: [],
+}
+
